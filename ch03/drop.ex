fbe67c3f6e7dcdf1d60ba7a6452d4d752593b695
abeyonalaja <abey.onalaja@gmail.com>
2014-10-17 15:54:03 +0100

    Pattern matching with tupples

--- ch03/drop.ex
+++ ch03/drop.ex
@@ -1,14 +1,14 @@
 defmodule Drop do
   
-  def fall_velocity(:earth, distance) do
-      :math.sqrt(2 * 9.8 * distance)
+  def fall_velocity(:earth, distance) when distance >= 0 do
+    :math.sqrt(2 * 9.8 * distance)
   end
 
-  def fall_velocity(:mars, distance) do
-    :math.sqrt(2 * 1.6 * distance)
+  def fall_velocity(:mars, distance) when distance >= 0 do
+    :math.sqrt( 2 *1.6 * distance)
   end
 
-  def fall_velocity(:moon, distance) do
-    :math.sqrt(2 * 3.71 * distance)
+  def fall_velocity(:moon, distance) when distance >= 0 do
+    :math.sqrt(2 * 3.17 * distance)
   end
 end
